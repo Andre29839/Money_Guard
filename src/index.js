05db4382b801a0a6c925f44a159e053915b7188f
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-11-03 16:18:35 +0200

    upgrade router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from 'components/App/App';
 import { persistor, store } from 'redux/configureStore';
 import { ThemeProvider } from 'styled-components';
@@ -16,10 +16,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={'/'}>
+        <HashRouter basename={'/'}>
           <App />
           <ToastContainer autoClose={2500} theme="dark" />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </ThemeProvider>
