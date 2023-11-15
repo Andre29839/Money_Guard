fc51f9642297184a2f12f17ff70c480da82c88a2
Andrii Nepomniashchyi <133494235+Andre29839@users.noreply.github.com>
2023-11-15 19:21:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/Money_Guard">
+        <BrowserRouter basename="/Money_Guard/">
           <App />
           <ToastContainer autoClose={2500} theme="dark" />
         </BrowserRouter>
@@ -26,3 +26,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </ThemeProvider>
 );
+
+console.log(BrowserRouter)
