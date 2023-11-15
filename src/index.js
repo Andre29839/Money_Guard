551b2716cf3119f87fd8b2cd2e027c68e7927d16
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-11-15 19:49:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter, useLocation } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import App from 'components/App/App';
@@ -14,18 +14,12 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'modern-normalize/modern-normalize.css';
 import './18n';
 
-function AppWithLocation() {
-  const location = useLocation();
-  console.log(location.pathname);
-  return <App />;
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/Money_Guard/">
-          <AppWithLocation />
+          <App />
           <ToastContainer autoClose={2500} theme="dark" />
         </BrowserRouter>
       </PersistGate>
