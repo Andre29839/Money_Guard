51199619ee70dfb995753540bb87b40f7b0d5ee5
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-11-20 22:50:15 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { refreshAuthThunk } from 'redux/registerReducers/registerThunks';
 
@@ -7,11 +7,13 @@ import {
   selectToken,
 } from 'redux/registerReducers/registerSelector';
 import UserRoutes from './UserRoutes';
+import Loader from 'components/Loader/Loader';
 
 function App() {
   const dispatch = useDispatch();
   const isLogin = useSelector(selectIsLogin);
   const token = useSelector(selectToken);
+  const [isLoading, setIsLoading] = useState(true);
 
   window.onbeforeunload = function () {
     localStorage.setItem('currentPath', window.location.pathname);
@@ -21,7 +23,12 @@ function App() {
     if (!isLogin && token) {
       dispatch(refreshAuthThunk());
     }
+    setIsLoading(false);
   }, [dispatch, isLogin, token]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return <UserRoutes />;
 }
